c1e5e7a0872312d0ca2ad4f61004e51e4aeff1bc
Bhoj GC <153856107+gcbhoj@users.noreply.github.com>
2024-01-23 18:16:13 -0500

    Add files via upload

--- TicketPriceCalculator.java
+++ TicketPriceCalculator.java
@@ -5,6 +5,31 @@ Child (age < 12): $5
 Adult (age >= 12 and age < 65): $10
 Senior (age >= 65): $7 */
 
+import java.util.Scanner;
+
 public class TicketPriceCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Yor Age for Ticket Price: ");
+        int age = scanner.nextInt();
+
+        if (age <= 0){
+            System.out.println("Invalid Age !. Please Enter a Valid Age.");
+        }else{
+            if(age < 12){
+                System.out.println(" For Kids Under 12 Price : $5 ");
+            }else if(age >=12 && age <65){
+                System.out.println("For Individulas between 12 jto 64 Price : $12");
+            }else {
+                System.out.println("For Seniors Price : $7");
+            }                    
+        }
+
+        scanner.close();
+
+
+        
+    }
 
 }
